07eca4651c960fedc9362f15cb4ccb417facb773
BT_4N2_12 <aoiofaoufhiua@gmail.com>
2023-11-13 13:56:46 -0500

    fixed names again

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         AdventureGame game1 = new AdventureGame();
-        System.out.println("Hello adventurer! Welcome to the 14th century. Times are rough now a days since the black plague. But luckily for us you're here,\nbefore we begin choose your class between these 3 options");
-        System.out.println("Class:\n Fighter: +30 damage\n -5 plague resistance\n -20 intelligence\n\n Doctor: +50 intelligence\n +10 plague resistance\n -10 damage\n\n Scavenger: +30 plague resistance\n-20 intelligence\n+10 damage");
+        System.out.println("Hello adventurer! Welcome to the 14th century. Times are rough nowadays since the black plague. But luckily for us you're here,\nbefore we begin choose your class between these 3 options");
+        System.out.println("Class:\n Barbarian: +30 damage\n -5 plague resistance\n -20 intelligence\n\n Doctor: +50 intelligence\n +10 plague resistance\n -10 damage\n\n Scavenger: +30 plague resistance\n-20 intelligence\n+10 damage");
         Scanner cahracterMaker = new Scanner(System.in);
-        System.out.println("\nPick a class: ");
+        System.out.println("\nPick a class(Type it exactly as it is): ");
         String newClass = cahracterMaker.nextLine();
         System.out.println("Now, what is your name?");
         String name = cahracterMaker.nextLine();
@@ -28,7 +28,7 @@ public class Main {
             if (chance == 1) {
                 System.out.println("You managed to narrowly escape with your life");
             } else {
-                System.out.println("You try running but to no avail! You must fight.");
+                System.out.println("You try running but to no avail! You must fight. (WIP)");
 
             }
         } else {
@@ -49,6 +49,7 @@ public class Main {
                 } else {
                     System.out.println("You try looting but eventually leave the wolf behind, ");
                 }
+            }
                 System.out.println("You keep traveling through the forest, until you hear a voice. The voice of a small child. Do you check it out? (y/n)");
                 Scanner choices = new Scanner(System.in);
                 String childChoice = choices.nextLine();
@@ -58,13 +59,15 @@ public class Main {
                     System.out.println("Looking at the child, you notice its lil billy, The towns trickster. Do you wish to save him?(y/n)");
                     Scanner lilBilly = new Scanner(System.in);
                     String saveChild = lilBilly.nextLine();
+                    System.out.println("WIP");
                     if(saveChild.contains("y") || saveChild.contains("Y")) {
-                        System.out.println("You go towards lil Billy, as he screams louder and louder, you notice that he isnt in trouble at all. In fact, the little turd wasnt in danger at all. \nAs you look into the distance, you see a looming figure grow bigger. What do you do next?(get closer or leave");
+                        System.out.println("You go towards lil Billy, as he screams louder and louder, you notice that he isnt in trouble at all. In fact, the little turd wasn't in danger at all. \nAs you look into the distance, you see a looming figure grow bigger. What do you do next?(get closer or leave)");
                     }
                 } else {
-                    System.out.println("You think carefully, screw that child! You head forwards and don't look back. " + "\u001B[33m" + "\n screw that kid! " + "\u001B[37m" + "you think to yourself");
+                    System.out.println("You think carefully, screw that child! You head forwards and don't look back. " + "\u001B[33m" + "\n screw that kid! ");
+                    System.out.print("\u001B[37m" + "you think to yourself");
                 }
-            }
+
         }
 
 
